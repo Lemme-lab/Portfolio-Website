337d388ed8253df6c18a0f314bf30a99ce174c7c
Schleichdie <Lucas.lenarcic@gmail.com>
2021-10-12 20:20:38 +0200

    release commit

--- Main-Page/randomthingiphp.php
+++ Main-Page/randomthingiphp.php
@@ -1,4 +1,6 @@
+
 <?php 
+if(!empty($_POST['website'])) die();
 $email = $_POST['email'];
 $fname = $_POST['firstname'];
 $lname = $_POST['lastname'];
@@ -10,4 +12,3 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
-
